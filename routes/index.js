bbb66acd80c71b6c8219f2b24faec1cd4133712b
Raul Val <raulval@outlook.com>
2021-11-18 20:24:58 -0300

    Criada a página de contato

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Tecnologias = require("../models/Tecnologias")
-const Informacoes = require("../models/Informacoes")
+const Tecnologias = require("../models/Tecnologias");
+const Informacoes = require("../models/Informacoes");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/tecnologias', function(req, res, next) {
-  res.render('tecnologias', { tecnologias:Tecnologias.tecnologias() });
-});
-
-router.get('/sobre', function(req, res, next) {
-  res.render('sobre');
+router.get("/tecnologias", function (req, res, next) {
+  res.render("tecnologias", { tecnologias: Tecnologias.tecnologias() });
 });
 
-router.get('/informacoes', function(req, res, next) {
-  res.render('informacoes',{informacoes : Informacoes.informacoes()});
+router.get("/sobre", function (req, res, next) {
+  res.render("sobre");
+});
+
+router.get("/informacoes", function (req, res, next) {
+  res.render("informacoes", { informacoes: Informacoes.informacoes() });
+});
+
+router.get("/contato", function (req, res, next) {
+  res.render("contato");
 });
 
 module.exports = router;
